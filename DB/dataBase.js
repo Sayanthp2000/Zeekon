8e880090fd5c61cad55e5e89fcb16332c07b52e9
Sayanthp2000 <psaya1852@gmail.com>
2024-06-18 17:06:56 +0530

    database change

--- DB/dataBase.js
+++ DB/dataBase.js
@@ -1,6 +1,6 @@
 const Mongoose = require("mongoose");
 
-const connctDB = Mongoose.connect("mongodb://127.0.0.1:27017/Zeekon");
+const connctDB = Mongoose.connect("mongodb+srv://psaya1852:YX9b5LjbbuT0Uhed@zeekon.ab4en1l.mongodb.net/?retryWrites=true&w=majority&appName=Zeekon");
 
 connctDB
     .then(()=> console.log("Database connected"))
